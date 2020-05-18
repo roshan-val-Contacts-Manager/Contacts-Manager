511335eb7f56ea076d62fc9de95edb54d50d8ee0
Valeria Meza <valeriameza173@gmail.com>
2020-05-18 09:12:55 -0500

    basic structure

---
+++ src/Contacts.java
@@ -0,0 +1,12 @@
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contacts {
+    public static void main(String[] args) {
+        String directory = "contacts";
+        String contactsFile = "contacts.txt";
+        List<String> contact = new ArrayList<>();
+
+    }
+}
